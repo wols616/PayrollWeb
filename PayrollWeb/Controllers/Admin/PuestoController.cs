25a0dcfcebbc3eddfb36228282b828058fbadd80
wols616 <wilber.rivas1@catolica.edu.sv>
2025-03-10 00:57:27 -0600

    Agregue los complementos por puesto

--- PayrollWeb/Controllers/Admin/PuestoController.cs
+++ PayrollWeb/Controllers/Admin/PuestoController.cs
@@ -9,7 +9,8 @@ namespace PayrollWeb.Controllers.Admin
     {
         Puesto _puesto = new Puesto();
         Categoria _categoria = new Categoria();
-        
+        Complemento_Puesto _complementoPuesto = new Complemento_Puesto();
+
         [Authorize]
         public IActionResult Index()
         {
@@ -37,6 +38,29 @@ namespace PayrollWeb.Controllers.Admin
             ViewBag.Categorias = _categoria.ObtenerCategorias();
             return View("/Views/Admin/EditarPuesto.cshtml", puesto);
         }
+
+        [Authorize]
+
+        public IActionResult VerComplementos(int id)
+        {
+            List<Complemento_Puesto> complementos = _complementoPuesto.ObtenerComplementosPuesto(id);
+            ViewBag.Puesto = _puesto.ObtenerPuesto(id);
+            return View("/Views/Admin/VerComplementos.cshtml", complementos);
+        }
+
+        [Authorize]
+        public IActionResult VerAgregarComplementos(int idPuesto)
+        {
+            ViewBag.Puesto = _puesto.ObtenerPuesto(idPuesto);
+            return View("/Views/Admin/AgregarComplemento.cshtml");
+        }
+        [Authorize]
+        public IActionResult VerEditarComplemento(int id)
+        {
+            Complemento_Puesto complemento = _complementoPuesto.ObtenerComplementoPorId(id);
+            return View("/Views/Admin/EditarComplemento.cshtml", complemento);
+        }
+
 
 
         //__________________________________________________________________________________________________________________________________
@@ -78,6 +102,55 @@ namespace PayrollWeb.Controllers.Admin
                 TempData["ErrorEliminar"] = "No se puede eliminar el puesto porque ya hay contratos asociados a este";
             }
             return RedirectToAction("VerPuestos");
+        }
+
+        public IActionResult CrearComplemento(string NombreComplemento, decimal Monto, int IdPuesto)
+        {
+            Complemento_Puesto complemento_Puesto = new Complemento_Puesto
+            {
+                NombreComplemento = NombreComplemento,
+                Monto = Monto,
+                IdPuesto = IdPuesto
+            };
+
+            if (!complemento_Puesto.AgregarComplemento())
+            {
+                TempData["Error"] = "Error al agregar el complemento";
+                return RedirectToAction("VerComplementos", new { id = IdPuesto });
+            }
+
+            TempData["Success"] = "Complemento agregado correctamente";
+            return RedirectToAction("VerComplementos", new { id = IdPuesto });
+        }
+
+        public IActionResult ActualizarComplemento(string NombreComplemento, decimal Monto, int IdPuesto, int IdComplementoPuesto)
+        {
+            Complemento_Puesto complemento = new Complemento_Puesto
+            {
+                NombreComplemento = NombreComplemento,
+                Monto = Monto,
+                IdPuesto = IdPuesto,
+                IdComplementoPuesto = IdComplementoPuesto
+            };
+
+            if (!complemento.ActualizarComplemento())
+            {
+                TempData["Error"] = "Error al actualizar el complemento";
+                return RedirectToAction("VerComplementos", new { id = IdPuesto });
+
+            }
+            TempData["Success"] = "Complemento actualizado correctamente";
+            return RedirectToAction("VerComplementos", new { id = IdPuesto });
+        }
+
+        public IActionResult EliminarComplemento(int id, int IdPuesto)
+        {
+            if (!_complementoPuesto.EliminarComplemento(id))
+            {
+                TempData["Error"] = "Error al eliminar el complemento";
+            }
+            TempData["Success"] = "Complemento eliminado correctamente";
+            return RedirectToAction("VerComplementos", new { id = IdPuesto });
         }
     }
 }
