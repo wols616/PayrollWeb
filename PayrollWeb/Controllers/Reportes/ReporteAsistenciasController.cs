3b9b212e6f5dedf2855708069645c4e0b8f2d00c
Donis004 <donisbrenda004@gmail.com>
2025-05-18 16:18:42 -0600

    Reporte de competencias_habilidades y asistencias hechas

---
+++ PayrollWeb/Controllers/Reportes/ReporteAsistenciasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PayrollWeb.Models;
+
+namespace PayrollWeb.Controllers.Reportes
+{
+    public class ReporteAsistenciasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult VerEmpleados()
+        {
+            return View("/Views/Reportes/VerEmpleadosAsistencias.cshtml", new Empleado().ObtenerEmpleados());
+        }
+
+        public IActionResult ReportesAsistenciasEmpleado(int IdEmpleado)
+        {
+            var reporte = new ReporteAsistencias().ObtenerAsistenciasEmpleado(IdEmpleado);
+            return View("/Views/Reportes/ReportesAsistencias.cshtml", reporte);
+        }
+    }
+}
